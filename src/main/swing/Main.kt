b1e4a47949f8556afd27b91f31eabb2305dc767d
wedro22 <wedro2208@gmail.com>
2019-01-20 16:43:26 +0300

    create table for explore components

--- src/main/swing/Main.kt
+++ src/main/swing/Main.kt
@@ -5,5 +5,8 @@ import java.awt.EventQueue
 lateinit var window : Window
 
 fun main(args: Array<String>) {
-    EventQueue.invokeLater { window = Window("swing") }
+    EventQueue.invokeLater {
+        window = Window("swing")
+        window.isVisible=true
+    }
 }
--- src/main/swing/Window.kt
+++ src/main/swing/Window.kt
@@ -1,21 +1,24 @@
 package main.swing
 
+import java.awt.GridLayout
 import javax.swing.*
 
-class Window(title: String) : JFrame(){
+class Window(title: String): JFrame(){
+    lateinit var table: JTable
 
     init {
         createUI(title)
-        isVisible=true
     }
 
     private fun createUI(title: String) {
-
         setTitle(title)
-
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         setSize(300, 200)
-        setLocationRelativeTo(null)
+        setLocationRelativeTo(null)//null=центр экрана
 
+        contentPane.layout = GridLayout(1,1)
+
+        table = JTable(10, 10)
+        contentPane.add(table)
     }
 }
