38de12f8fba9dbc423e49bd2bee43ad974bba547
wedro22 <wedro2208@gmail.com>
2019-01-24 01:28:41 +0300

    is drawing

--- src/main/swing/Main.kt
+++ src/main/swing/Main.kt
@@ -3,10 +3,15 @@ package main.swing
 import java.awt.EventQueue
 
 lateinit var window : Window
+var scopes = 0
+val size=10
+enum class Cell{NONE, SNAKE, MOUSE}
 
 fun main(args: Array<String>) {
     EventQueue.invokeLater {
         window = Window("swing")
-        window.isVisible=true
     }
+    pole[2][3] = Cell.MOUSE
 }
+
+val pole = Array(size, {Array(size, {Cell.NONE})})
--- src/main/swing/Window.kt
+++ src/main/swing/Window.kt
@@ -1,24 +1,79 @@
 package main.swing
 
-import java.awt.GridLayout
+import java.awt.*
 import javax.swing.*
+import main.swing.pole
 
 class Window(title: String): JFrame(){
-    lateinit var table: JTable
 
     init {
         createUI(title)
+        pack()
+        isResizable = false
+        setTitle(title)
+        defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        setLocationRelativeTo(null)//null=центр экрана
+        isVisible = true
     }
 
     private fun createUI(title: String) {
-        setTitle(title)
-        defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        setSize(300, 200)
-        setLocationRelativeTo(null)//null=центр экрана
+        contentPane.layout = BorderLayout()
+        //contentPane.layout = GridBagLayout()
 
-        contentPane.layout = GridLayout(1,1)
+        val panel = MyJPanel()
+        val panelSize = Dimension(300, 300)
+        panel.preferredSize = panelSize
+        panel.maximumSize = panelSize
+        panel.minimumSize = panelSize
+        panel.border = BorderFactory.createLineBorder(Color.BLACK)
+        add(panel)
+        add(JPanel(), BorderLayout.EAST)
+        add(JPanel(), BorderLayout.WEST)
+        add(JPanel(), BorderLayout.NORTH)
 
-        table = JTable(10, 10)
-        contentPane.add(table)
+        val buttonPan = JPanel(FlowLayout(FlowLayout.CENTER))
+            val labelScope = JLabel("очки: $scopes", SwingConstants.CENTER)
+            labelScope.preferredSize = Dimension(300, 20)
+            buttonPan.add(labelScope)
+        add(buttonPan, BorderLayout.SOUTH)
+    }
+
+
+
+    class MyJPanel: JPanel() {
+        var cellSize = 0
+
+        override fun paint(g: Graphics?) {
+            super.paint(g)
+            if ( g == null || width <= 0 || height <= 0 ) return
+            cellSize = width/pole.size
+            drawPole(g)
+            drawAll(g)
+        }
+
+        fun drawPole(g: Graphics){
+            g.color = Color.blue
+            for (i in 1..pole.size-1){
+                g.drawLine(0, i*cellSize, width, i*cellSize)
+                g.drawLine(i*cellSize, 0, i*cellSize, height)
+            }
+        }
+
+        fun drawAll(g:Graphics){
+            for (y in 0..pole.size-1){
+                for (x in 0.. pole.size-1){
+                    drawCell(g, x, y)
+                }
+            }
+        }
+
+        fun drawCell(g: Graphics, x: Int, y: Int){
+            g.color = when (pole[x][y]){
+                Cell.NONE -> Color.WHITE
+                Cell.MOUSE -> Color.GREEN
+                Cell.SNAKE -> Color.ORANGE
+            }
+            g.fillRect(x*cellSize+1, y*cellSize+1, cellSize-2, cellSize-2)
+        }
     }
 }
