e76a68041531c5af0171f5b76e3740a2b34589d4
wedro22 <wedro2208@gmail.com>
2019-01-26 01:13:22 +0300

    alpha. not ready restart. bed code

--- src/main/swing/Main.kt
+++ src/main/swing/Main.kt
@@ -1,17 +1,81 @@
 package main.swing
 
-import java.awt.EventQueue
+import tornadofx.move
+import java.awt.*
+import kotlin.concurrent.timer
 
-lateinit var window : Window
+enum class Cell(val color: Color){NONE(Color.WHITE), SNAKE(Color.RED), MOUSE(Color.GREEN)}
+enum class Side(var x: Int, var y: Int){
+    STOP(0, 0), UP(0, -1), DOWN(0, 1), LEFT(-1, 0), RIGHT(1, 0)
+}
 var scopes = 0
-val size=10
-enum class Cell{NONE, SNAKE, MOUSE}
+val SIZE = 10
+val snake = Snake()
+val feed = Feed()
+var side = Side.STOP
+var oldSide = side
+lateinit var window: Window
+var endGame = false
 
 fun main(args: Array<String>) {
-    EventQueue.invokeLater {
-        window = Window("swing")
-    }
-    pole[(0..size-1).random()][(0..size-1).random()] = Cell.MOUSE
+    /*EventQueue.invokeLater {
+        window = Window("Змейка")
+    }*/
+    window = Window("Змейка")
+    timer(null, true, 0, 300, action = {
+        if ((side != Side.STOP) and !endGame){
+            snake.go()
+            window.redraw()
+        }
+    })
 }
 
-val pole = Array(size, {Array(size, {Cell.NONE})})
+class Snake {
+    val list = arrayListOf(
+        Point(SIZE/2, SIZE/2),
+        Point(SIZE/2, SIZE/2+1),
+        Point(SIZE/2, SIZE/2+2))
+    private lateinit var last: Point
+    private lateinit var first: Point
+    val future = Point(0, 0)
+
+    fun go(){
+        last = snake.list.last()    //хвост
+        first = snake.list.first()  //голова
+        future.move(first.x+side.x, first.y+side.y) //ожидаемая новая позиция головы
+        if (future.x<0 || future.x>=SIZE || future.y<0 || future.y>= SIZE   //вляпались в край поля
+            || (list.contains(future) and (future.location!=list[1]) and (future.location!=last))) {  //вляпались в себя
+            endGame()
+            return
+        }
+        if (future.location == list[1].location) { //нельзя ползти назад
+            side = oldSide  //меняем направление на предыдущее
+            future.move(first.x+side.x, first.y+side.y) //новая позиция головы будет по старому направлению
+        }
+        last.location = future.location  //смена координат последнего блока на будущего первого
+        snake.list.move(last, 0)    //последний блок -> первый блок
+        oldSide = side  //запоминаем новое направление
+        if (last.location == feed.point.location) {   //если поели
+            scopes++
+            feed.new()  //теперь еда в другом месте
+            var last2 = Point(list.last().x, list.last().y) //запоминаем где был хвост
+            go()    //еще растем вперед
+            list.add(last2)     //увеличиваем на 1 блок
+        }
+    }
+}
+
+class Feed {
+    private fun randomPoint() = Point((0..SIZE-1).random(), (0..SIZE-1).random())
+    var point = randomPoint()
+    fun new(){
+        while (snake.list.contains(point)) point = randomPoint()
+    }
+    constructor(){
+        new()
+    }
+}
+
+fun endGame(){
+    endGame = true
+}
--- src/main/swing/Window.kt
+++ src/main/swing/Window.kt
@@ -1,79 +1,113 @@
 package main.swing
 
 import java.awt.*
+import java.awt.event.KeyEvent.*
+import java.awt.event.KeyEvent
+import java.awt.event.KeyListener
 import javax.swing.*
-import main.swing.pole
 
-class Window(title: String): JFrame(){
+class Window(title: String): JFrame(), KeyListener{
+
+    override fun keyPressed(e: KeyEvent?) {
+        if (e == null) return
+        if (e.keyCode == VK_ESCAPE)
+            System.exit(0)
+        side = when (e.keyCode) {
+            VK_UP, VK_W -> Side.UP
+            VK_DOWN, VK_S -> Side.DOWN
+            VK_LEFT, VK_A -> Side.LEFT
+            VK_RIGHT, VK_D -> Side.RIGHT
+            else -> side
+        }
+    }
+
+    override fun keyReleased(e: KeyEvent?) {
+
+    }
+
+    override fun keyTyped(e: KeyEvent?) {
+
+    }
 
     init {
-        createUI(title)
+        isUndecorated = true
+
+        createUI()
         pack()
         isResizable = false
         setTitle(title)
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         setLocationRelativeTo(null)//null=центр экрана
         isVisible = true
+
+        addKeyListener(this)
+        isFocusable = true
+        //focusableWindowState = true
+        focusTraversalKeysEnabled = false
     }
 
-    private fun createUI(title: String) {
-        contentPane.layout = BorderLayout()
-        //contentPane.layout = GridBagLayout()
+    private lateinit var panel: MyJPanel
+    private lateinit var labelScope: JLabel
 
-        val panel = MyJPanel()
+    private fun createUI() {
+        contentPane.layout = BorderLayout()
+
+        panel = MyJPanel()
         val panelSize = Dimension(300, 300)
         panel.preferredSize = panelSize
         panel.maximumSize = panelSize
         panel.minimumSize = panelSize
         panel.border = BorderFactory.createLineBorder(Color.BLACK)
+        panel.background = Color.WHITE
         add(panel)
         add(JPanel(), BorderLayout.EAST)
         add(JPanel(), BorderLayout.WEST)
         add(JPanel(), BorderLayout.NORTH)
 
         val buttonPan = JPanel(FlowLayout(FlowLayout.CENTER))
-            val labelScope = JLabel("очки: $scopes", SwingConstants.CENTER)
+            labelScope = JLabel("очки: $scopes", SwingConstants.CENTER)
             labelScope.preferredSize = Dimension(300, 20)
             buttonPan.add(labelScope)
         add(buttonPan, BorderLayout.SOUTH)
     }
 
+    fun redraw(){
+        panel.repaint()
+        labelScope.text = "очки: $scopes"
+    }
 
-
-    class MyJPanel: JPanel() {
+    private class MyJPanel: JPanel() {
         var cellSize = 0
 
         override fun paint(g: Graphics?) {
             super.paint(g)
             if ( g == null || width <= 0 || height <= 0 ) return
-            cellSize = width/pole.size
+            cellSize = width/SIZE
             drawPole(g)
-            drawAll(g)
+            drawSnake(g)
+            drawFeed(g)
         }
 
-        fun drawPole(g: Graphics){
+        private fun drawPole(g: Graphics){
             g.color = Color.blue
-            for (i in 1..pole.size-1){
+            for (i in 1..SIZE-1){
                 g.drawLine(0, i*cellSize, width, i*cellSize)
                 g.drawLine(i*cellSize, 0, i*cellSize, height)
             }
         }
 
-        fun drawAll(g:Graphics){
-            for (y in 0..pole.size-1){
-                for (x in 0.. pole.size-1){
-                    drawCell(g, x, y)
-                }
-            }
+        private fun drawSnake(g: Graphics){
+            snake.list.forEach { point -> drawCell(g, Cell.SNAKE, point.x, point.y) }
         }
 
-        fun drawCell(g: Graphics, x: Int, y: Int){
-            g.color = when (pole[x][y]){
-                Cell.NONE -> Color.WHITE
-                Cell.MOUSE -> Color.GREEN
-                Cell.SNAKE -> Color.ORANGE
-            }
-            g.fillRect(x*cellSize+1, y*cellSize+1, cellSize-2, cellSize-2)
+        private fun drawFeed(g: Graphics){
+            drawCell(g, Cell.MOUSE, feed.point.x, feed.point.y)
+        }
+
+        private fun drawCell(g: Graphics, cell: Cell, x: Int, y: Int){
+            g.color = cell.color
+            g.fillRect(x*cellSize+1, y*cellSize+1, cellSize-1, cellSize-1)
         }
     }
+
 }
